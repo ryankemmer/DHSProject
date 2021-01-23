5383e63cb9b8efb4cd3462b8cb3dd4130a80d2f6
Ryan Kemmer <ryankemmer77@gmail.com>
2021-01-22 17:21:00 -0700

    WIP

--- dataOps/aggregationMethods.py
+++ dataOps/aggregationMethods.py
@@ -346,6 +346,7 @@ for i in range(24):
 
 print(str("---------------------- Totals ------------------------"))
 
+'''
 print('Majority Vote Totals: ' + sum())
 
 print('Confidence-Weighted Winner: ' + )
@@ -359,7 +360,7 @@ print('Honeypot Winner: ' + )
 print('Weighted Honeypot Winner: ' + )
 
 print('ELICE: ' + )
-
+'''
 
 
          
