86bc9e6e84ce6e14a6be79d243c6d54bf4aa3bab
HarikaBhogaraju <harika.bhogaraju@gmail.com>
2021-08-12 21:24:33 +0000

    changes

--- dataOps/individualUser.py
+++ dataOps/individualUser.py
@@ -3,6 +3,7 @@ import json
 import time
 import datetime
 import sys
+import numpy
 
 #url = 'mongodb://localhost:27017/'
 url = 'mongodb://localhost:27014/'
@@ -16,6 +17,7 @@ usersCol = db['users']
 responsesCol = db['responses']
 percentArray = []
 dataArray = []
+fnrArray = []
 #specify ground truth for each question
 groundtruth = [0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1]
 
@@ -33,8 +35,40 @@ for user in usersCol.find():
         percentArray.append(score*100/24)
 
     for x in userResponse:
-        if(x["question"] > 10 and x["q1"] != 1):
+        if(x["question"] > 11 and x["q1"] != 1):
             fnr = fnr+1
+    
     print(fnr)
-    print("FNR = ",(fnr*100/10))
+    if(fnr>13):
+        fnrArray.append(100)
+    else:
+        fnrArray.append((fnr*100/13))
+
+    print("FNR = ",(fnr*100/13))
 print(percentArray)
+print(fnrArray)
+
+#average percentage
+sum = 0
+for i in range(len(percentArray)):
+    sum = sum + percentArray[i]
+
+avg = sum/len(percentArray)
+array = numpy.array(percentArray)
+print("Min : " + str(numpy.min(array)))
+print("Max : " + str(numpy.max(array)))
+print("Average Score %" + str(avg))
+
+#average fnr percentage
+sum = 0
+for i in range(len(fnrArray)):
+    if(fnrArray[i] > 100):
+        sum = sum+100
+    else:
+        sum = sum + fnrArray[i]
+
+avg = sum/len(fnrArray)
+array = numpy.array(fnrArray)
+print("Min : " + str(numpy.min(array)))
+print("Max : " + str(numpy.max(array)))
+print("Average FNR  %" + str(avg))
