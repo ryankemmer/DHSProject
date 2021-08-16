f113375c438d595389c3daa88ef27f8d51642064
HarikaBhogaraju <harika.bhogaraju@gmail.com>
2021-08-15 17:59:19 -0700

    bb analysis update

--- dataOps/bbAnalysis.py
+++ dataOps/bbAnalysis.py
@@ -17,10 +17,11 @@ usersCol = db['users']
 responsesCol = db['responses']
 percentArray = []
 dataArray = []
-userStartX = [];
-userStartY = [];
-userEndX = [];
-userEndY = [];
+
+bbStartX = []
+
+
+
 
 #specify ground truth for each question
 groundtruth = [0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1]
@@ -29,6 +30,8 @@ for user in usersCol.find():
 
     userName = user['user']
     userResponse = responsesCol.find({"user":userName})
+    userBBCount = 0
+
     #print(userResponse["q1"])
     print(userName)
     demographic = user["surveyResults"]
@@ -40,8 +43,27 @@ for user in usersCol.find():
 
     for i in range(1,25):
         print(i," = LOOP ENTERED")
+
         response = responsesCol.find_one({"user": userName, "question": i})
         print(response["boundingBox"])
 
         if(response["boundingBox"]["startX"] != None):
-            print("")
+            if(response["boundingBox"]["w"] < 0 || response["boundingBox"]["h"] < 0):
+                userStartX = response["boundingBox"]["startX"] + response["boundingBox"]["w"]
+                userStartY = response["boundingBox"]["startY"] + response["boundingBox"]["h"]
+                userEndX = response["boundingBox"]["startX"]
+                userEndY = response["boundingBox"]["startY"]
+
+                if(userStartX <= bbCoordinates[j]+10 or userStartX >= bbCoordinates[j]-10 and userEndX <= bbCoordinates[j]+10 or userEndX >= bbCoordinates[j]-10 and userStartY <= bbCoordinates[j]+10 or userStartY >= bbCoordinates[j]-10 and userEndY <= bbCoordinates[j]+10 or userEndY >= bbCoordinates[j]-10):
+                    userBBCount = userBBCount + 1
+            else:
+                userStartX = response["boundingBox"]["startX"]
+                userStartY = response["boundingBox"]["startY"]
+                userEndX = response["boundingBox"]["startX"] + response["boundingBox"]["w"]
+                userEndY = response["boundingBox"]["startY"] + response["boundingBox"]["h"]
+
+                if(userStartX <= bbStartX[j]+10 or userStartX >= bbStartX[j]-10 and userEndX <= bbEndX[j]+10 or userEndX >= bbEndX[j]-10 and userStartY <= bbStartY[j]+10 or userStartY >= bbStartY[j]-10 and userEndY <= bbEndY[j]+10 or userEndY >= bbEndY[j]-10):
+                    userBBCount = userBBCount + 1
+
+    print(bbCount)
+    usersBB.append(bbCount)
