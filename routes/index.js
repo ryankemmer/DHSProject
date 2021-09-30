28982d7b33bf899fe50f12e4e784648f1ab1fd46
HarikaBhogaraju <harika.bhogaraju@gmail.com>
2021-09-30 12:40:48 -0700

    changes to border of image

--- routes/index.js
+++ routes/index.js
@@ -133,7 +133,7 @@ router.post('/activity/:userID/', function(req,res,next){
       }
       else{
         //change Ground Truth Array
-        var truth = [1,1,1,1,2,2,2,2,3,3,3,3,0,0,0,0,0,0,0,0,0,0,0,0] //24 in length
+        var truth = [1,1,1,2,2,2,2,3,3,3,3,1,0,0,0,0,0,0,0,0,0,0,0,0] //24 in length
         var correct = []
 
         //get results
