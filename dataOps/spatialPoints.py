7717d49f39ea92c642ffdb010fe9e325f8bdc491
HarikaBhogaraju <harika.bhogaraju@gmail.com>
2021-11-11 14:03:20 -0700

    new file

---
+++ dataOps/spatialPoints.py
@@ -0,0 +1,7 @@
+import cv2
+
+mouseinfo = [[52.7, 487, 454], [52.5, 443, 368], [52.4, 327, 230], [52.2, 303, 216], [52, 305, 250], [51.8, 305, 256]]
+for mi in mouseinfo:
+    x = mouseinfo[0]
+    y = mouseinfo[2]
+    image = cv2.circle(image, (x,y), radius=0, color=(0, 0, 255), thickness=-1)
